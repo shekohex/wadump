96cf24b4de1c7b1931a14974c8a65ed0f2a894e2
Shady Khalifa <shekohex@gmail.com>
2020-07-28 14:57:48 +0200

    use cargo instead of cross

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,14 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cargo build --target $TARGET
+    cargo build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cargo test --target $TARGET
 }
 
 # we don't run the "test phase" when doing deploys
